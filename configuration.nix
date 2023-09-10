142c5ef9178060a1befbac21f7cfd6eb22f5320d
Kai Mies <develop@regnavi.de>
2023-09-10 07:00:42 +0200

    Fix

--- configuration.nix
+++ configuration.nix
@@ -2,11 +2,16 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.binfmt.emulatedSystems = ["armv7l-linux"];
+
   users.users = {
     kmies = {
       isNormalUser = true;
       home = "/home/kmies";
-      openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJGb5cPrpHA5mLxknm0WInP6NuZylMCE6Z9LT+IRT7J4 kmies@zeus"];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJGb5cPrpHA5mLxknm0WInP6NuZylMCE6Z9LT+IRT7J4 kmies@zeus"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOQfx0MkikNczT1S4grgeN+h9YidWhJjG1XcsCiNcFlb kmies@demeter"
+      ];
       initialPassword = "1";
       extraGroups = ["wheel"];
     };
